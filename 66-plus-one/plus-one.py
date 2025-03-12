40b74f4feabd5f4ce177904b7e4e705ad31821fb
Yashraj Singh <contact@yashrajs.com>
2025-03-12 09:26:17 +0530

    Time: 2 ms (11.84%) | Memory: 12.5 MB (52.16%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        number = int("".join(map(str, digits)))
+        number += 1
+        return [int(x) for x in str(number)]
